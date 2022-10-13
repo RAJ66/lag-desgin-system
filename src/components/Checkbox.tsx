2775bc59d74c10d97809ece7603d7332e985a76a
vitor.barbosa <vitor.barbosa@sovos.com>
2022-10-13 15:23:30 +0100

    improve app

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,17 +1,16 @@
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
-import {Check} from "phosphor-react"
-export interface CheckboxProps {
-}
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { Check } from "phosphor-react";
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
 
-export function Checkbox() {
+export function Checkbox(props: CheckboxProps) {
   return (
-    <CheckboxPrimitive.Root className='w-6 h-6 p-[2px] bg-gray-800 rounded'>
-    <CheckboxPrimitive.Indicator asChild>
-      <Check className='h-5 w-5 text-cyan-500' weight='bold'/>
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
+    <CheckboxPrimitive.Root
+      className="w-6 h-6 p-[2px] bg-gray-800 rounded "
+      {...props}
+    >
+      <CheckboxPrimitive.Indicator asChild>
+        <Check className="h-5 w-5 text-cyan-500" weight="bold" />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
   );
 }
-
-
-
